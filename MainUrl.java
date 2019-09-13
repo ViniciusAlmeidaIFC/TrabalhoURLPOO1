58a9bb0cc76b6376d73c21863894de93664e5a89
CaioBervian <52167435+CaioBervian@users.noreply.github.com>
2019-09-13 09:24:26 -0400

    Add files via upload

---
+++ MainUrl.java
@@ -0,0 +1,11 @@
+
+public class MainUrl {
+
+	public static void main(String[] args) {
+		// Feito por Caio Luiz Bervian,Vinicius Goulart Almeida e Thomas W Manzi
+	Url url = new Url("https://www.urionlinejudge.com.br/judge/pt/profile/374528");
+		System.out.println(url);
+	
+	}
+
+}
